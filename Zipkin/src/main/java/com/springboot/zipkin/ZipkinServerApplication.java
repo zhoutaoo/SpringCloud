e0ea70c72187b946c280fcc8e394f9663f1d99d2
周涛 <zhotuaoo@foxmail.com>
2017-07-26 23:05:01 +0800

    zipkin改为消息队列

--- Zipkin/src/main/java/com/springboot/zipkin/ZipkinServerApplication.java
+++ Zipkin/src/main/java/com/springboot/zipkin/ZipkinServerApplication.java
@@ -3,11 +3,11 @@ package com.springboot.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
