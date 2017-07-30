2da6330865c45d65b3e8a4e988a9e481ecf9fddc
周涛 <zhotuaoo@foxmail.com>
2017-07-30 19:07:11 +0800

    加上断言

--- Producer/src/test/java/com/springboot/producer/service/UserServiceTest.java
+++ Producer/src/test/java/com/springboot/producer/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.springboot.producer.service;
 
 import com.springboot.producer.entity.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ public class UserServiceTest {
         User user = new User();
         user.setName("王五");
         user.setUpdatedDate(new Date());
-        userService.add(user);
+        Assert.assertEquals(1, userService.add(user));
     }
 
 }
