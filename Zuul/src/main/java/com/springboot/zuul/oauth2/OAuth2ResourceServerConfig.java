c961215c5838777f7edc6c20b7427655f99cba56
周涛 <zhotuaoo@foxmail.com>
2017-08-13 22:51:45 +0800

    添加oauth2

---
+++ Zuul/src/main/java/com/springboot/zuul/oauth2/OAuth2ResourceServerConfig.java
@@ -0,0 +1,18 @@
+package com.springboot.zuul.oauth2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class OAuth2ResourceServerConfig extends GlobalMethodSecurityConfiguration {
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        return new OAuth2MethodSecurityExpressionHandler();
+    }
+}
---
+++ Zuul/src/main/java/com/springboot/zuul/rest/FooController.java
@@ -0,0 +1,19 @@
+package com.springboot.zuul.rest;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.apache.commons.lang.RandomStringUtils.randomNumeric;
+
+@RestController
+public class FooController {
+
+    @PreAuthorize("#oauth2.hasScope('read')")
+    @RequestMapping(method = RequestMethod.GET, value = "/foos/{id}")
+    public String findById(@PathVariable long id) {
+        return "aa"+randomNumeric(2);
+    }
+}
