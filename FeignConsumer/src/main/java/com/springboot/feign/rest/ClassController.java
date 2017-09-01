48dfeede4b3fcfbdb53bbb20a0dff857c5f59421
周涛 <zhotuaoo@foxmail.com>
2017-09-01 22:57:29 +0800

    重构模块

--- FeignConsumer/src/main/java/com/springboot/feign/rest/ClassController.java
+++ FeignConsumer/src/main/java/com/springboot/feign/rest/ClassController.java
@@ -2,12 +2,14 @@ package com.springboot.feign.rest;
 
 import com.springboot.feign.service.ClassService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClassController {
 
+    @Qualifier("eureka-producer")
     @Autowired
     ClassService classService;
 
