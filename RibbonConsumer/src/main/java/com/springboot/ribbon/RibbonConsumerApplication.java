67f38cdf6f07ad30989b277cfca08b52fc2f3b9e
周涛 <zhotuaoo@foxmail.com>
2017-09-02 22:36:03 +0800

    调ribbon相关内容

--- RibbonConsumer/src/main/java/com/springboot/ribbon/RibbonConsumerApplication.java
+++ RibbonConsumer/src/main/java/com/springboot/ribbon/RibbonConsumerApplication.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableEurekaClient
 @EnableCircuitBreaker
 public class RibbonConsumerApplication {
 
