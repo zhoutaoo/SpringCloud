396828e28f0d916759107822bd3da18733ab9cb0
周涛 <zhotuaoo@foxmail.com>
2018-03-17 23:02:14 +0800

    增加plugin库

--- Zipkin/src/main/java/com/springboot/zipkin/ZipkinServerApplication.java
+++ Zipkin/src/main/java/com/springboot/zipkin/ZipkinServerApplication.java
@@ -3,10 +3,11 @@ package com.springboot.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableZipkinServer
+@EnableZipkinServer
 public class ZipkinServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
