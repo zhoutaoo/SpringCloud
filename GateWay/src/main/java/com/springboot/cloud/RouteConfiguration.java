7fca73893a2ec98e8be210a83ffb9022428743d6
周涛 <zhoutaoo@foxmail.com>
2018-04-10 18:10:10 +0800

    网关丰富功能，转向producer

---
+++ GateWay/src/main/java/com/springboot/cloud/RouteConfiguration.java
@@ -0,0 +1,15 @@
+package com.springboot.cloud;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfiguration {
+    @Bean
+    public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient);
+    }
+}
