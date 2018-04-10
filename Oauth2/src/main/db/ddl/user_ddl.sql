dd69f573215946c254d8c051e90d44b2aae19753
周涛 <zhoutaoo@foxmail.com>
2018-04-10 23:22:10 +0800

    增加docker容器

--- Oauth2/src/main/db/ddl/user_ddl.sql
+++ Oauth2/src/main/db/ddl/user_ddl.sql
@@ -5,7 +5,7 @@ CREATE TABLE users
   username                VARCHAR(100),
   password                VARCHAR(100),
   name                    VARCHAR(256),
-  moblie                  VARCHAR(20),
+  mobile                  VARCHAR(20),
   enabled                 BOOLEAN,
   account_non_expired     BOOLEAN,
   credentials_non_expired BOOLEAN,
@@ -20,13 +20,13 @@ SELECT setval('users_id_seq', 1000000);
 CREATE UNIQUE INDEX ux_users_username
   ON users (username);
 CREATE UNIQUE INDEX ux_users_mobile
-  ON users (moblie);
+  ON users (mobile);
 
 COMMENT ON COLUMN users.id IS '用户id';
 COMMENT ON COLUMN users.username IS '用户名';
 COMMENT ON COLUMN users.password IS '用户密码密文';
 COMMENT ON COLUMN users.name IS '用户姓名';
-COMMENT ON COLUMN users.moblie IS '用户手机';
+COMMENT ON COLUMN users.mobile IS '用户手机';
 COMMENT ON COLUMN users.enabled IS '是否有效用户';
 COMMENT ON COLUMN users.account_non_expired IS '账号是否未过期';
 COMMENT ON COLUMN users.credentials_non_expired IS '密码是否未过期';
--- Oauth2/src/main/db/dml/user_dml.sql
+++ Oauth2/src/main/db/dml/user_dml.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.users (id, username, password, enabled, account_non_expired, credentials_non_expired, account_non_locked, name, moblie, created_time, updated_time, created_by, updated_by)
+INSERT INTO public.users (id, username, password, enabled, account_non_expired, credentials_non_expired, account_non_locked, name, mobile, created_time, updated_time, created_by, updated_by)
 VALUES
   (1000001, 'zhoutaoo', '$2a$10$vYA9wKn/hVGOtwQw2eHiceeIGNBdfLYpDmbzHgBSVmOfHXPH4iYdS', 'true', 'true', 'true', 'true',
             ' 周涛', 15619841000, '2017-08-20 12:40:27.902294',
