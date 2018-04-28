c6e2e935f9ae5bd2e4e7d10a48a149b46538f03c
周涛 <zhoutaoo@foxmail.com>
2018-04-28 22:29:30 +0800

    加入岗位、角色、组织架构、菜单、资源等关系表，基础管理功能实体

---
+++ auth/auth-oauth2/src/main/db/ddl/users_ddl.sql
@@ -0,0 +1,273 @@
+--用户表
+DROP TABLE IF EXISTS users;
+CREATE TABLE users
+(
+  id                      SERIAL PRIMARY KEY,
+  username                VARCHAR(100) NOT NULL,
+  password                VARCHAR(100) NOT NULL,
+  name                    VARCHAR(200),
+  mobile                  VARCHAR(20),
+  enabled                 BOOLEAN,
+  account_non_expired     BOOLEAN,
+  credentials_non_expired BOOLEAN,
+  account_non_locked      BOOLEAN,
+  created_time            TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time            TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by              VARCHAR(100) NOT NULL,
+  updated_by              VARCHAR(100) NOT NULL
+);
+SELECT setval('users_id_seq', 1000000);
+CREATE UNIQUE INDEX ux_users_username
+  ON users (username);
+CREATE UNIQUE INDEX ux_users_mobile
+  ON users (mobile);
+COMMENT ON TABLE users IS '用户表';
+COMMENT ON COLUMN users.id IS '用户id';
+COMMENT ON COLUMN users.username IS '用户名';
+COMMENT ON COLUMN users.password IS '用户密码密文';
+COMMENT ON COLUMN users.name IS '用户姓名';
+COMMENT ON COLUMN users.mobile IS '用户手机';
+COMMENT ON COLUMN users.enabled IS '是否有效用户';
+COMMENT ON COLUMN users.account_non_expired IS '账号是否未过期';
+COMMENT ON COLUMN users.credentials_non_expired IS '密码是否未过期';
+COMMENT ON COLUMN users.created_time IS '创建时间';
+COMMENT ON COLUMN users.updated_time IS '更新时间';
+COMMENT ON COLUMN users.created_by IS '创建人';
+COMMENT ON COLUMN users.updated_by IS '更新人';
+
+--用户组表
+DROP TABLE IF EXISTS groups;
+CREATE TABLE groups
+(
+  id           SERIAL PRIMARY KEY,
+  parent_id    SERIAL       NOT NULL,
+  name         VARCHAR(200),
+  descrition   VARCHAR(500),
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('groups_id_seq', 1000000);
+COMMENT ON TABLE groups IS '用户组表';
+COMMENT ON COLUMN groups.id IS '用户组id';
+COMMENT ON COLUMN groups.parent_id IS '用户组父id';
+COMMENT ON COLUMN groups.name IS '用户组名称';
+COMMENT ON COLUMN groups.descrition IS '用户组简介';
+COMMENT ON COLUMN groups.created_time IS '创建时间';
+COMMENT ON COLUMN groups.updated_time IS '更新时间';
+COMMENT ON COLUMN groups.created_by IS '创建人';
+COMMENT ON COLUMN groups.updated_by IS '更新人';
+
+--用户和组关系表
+DROP TABLE IF EXISTS users_groups_relation;
+CREATE TABLE users_groups_relation
+(
+  id           SERIAL PRIMARY KEY,
+  user_id      SERIAL       NOT NULL,
+  group_id     SERIAL       NOT NULL,
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('users_groups_relation_id_seq', 1000000);
+COMMENT ON TABLE users_groups_relation IS '用户和组关系表';
+COMMENT ON COLUMN users_groups_relation.id IS '关系id';
+COMMENT ON COLUMN users_groups_relation.user_id IS '用户id';
+COMMENT ON COLUMN users_groups_relation.group_id IS '用户组id';
+COMMENT ON COLUMN users_groups_relation.created_time IS '创建时间';
+COMMENT ON COLUMN users_groups_relation.updated_time IS '更新时间';
+COMMENT ON COLUMN users_groups_relation.created_by IS '创建人';
+COMMENT ON COLUMN users_groups_relation.updated_by IS '更新人';
+
+--岗位表
+DROP TABLE IF EXISTS positions;
+CREATE TABLE positions
+(
+  id           SERIAL PRIMARY KEY,
+  name         VARCHAR(200),
+  descrition   VARCHAR(500),
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('positions_id_seq', 1000000);
+COMMENT ON TABLE positions IS '岗位表';
+COMMENT ON COLUMN positions.id IS '岗位id';
+COMMENT ON COLUMN positions.name IS '岗位名称';
+COMMENT ON COLUMN positions.descrition IS '岗位简介';
+COMMENT ON COLUMN positions.created_time IS '创建时间';
+COMMENT ON COLUMN positions.updated_time IS '更新时间';
+COMMENT ON COLUMN positions.created_by IS '创建人';
+COMMENT ON COLUMN positions.updated_by IS '更新人';
+
+--用户和角色系表
+DROP TABLE IF EXISTS users_positions_relation;
+CREATE TABLE users_positions_relation
+(
+  id           SERIAL PRIMARY KEY,
+  user_id      SERIAL       NOT NULL,
+  position_id  SERIAL       NOT NULL,
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('users_positions_relation_id_seq', 1000000);
+COMMENT ON TABLE users_positions_relation IS '用户和角色关系表';
+COMMENT ON COLUMN users_positions_relation.id IS '关系id';
+COMMENT ON COLUMN users_positions_relation.user_id IS '用户id';
+COMMENT ON COLUMN users_positions_relation.position_id IS '角色id';
+COMMENT ON COLUMN users_positions_relation.created_time IS '创建时间';
+COMMENT ON COLUMN users_positions_relation.updated_time IS '更新时间';
+COMMENT ON COLUMN users_positions_relation.created_by IS '创建人';
+COMMENT ON COLUMN users_positions_relation.updated_by IS '更新人';
+
+--角色表
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles
+(
+  id           SERIAL PRIMARY KEY,
+  code         VARCHAR(100) NOT NULL,
+  name         VARCHAR(200),
+  descrition   VARCHAR(500),
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('roles_id_seq', 1000000);
+COMMENT ON TABLE roles IS '角色表';
+COMMENT ON COLUMN roles.id IS '角色id';
+COMMENT ON COLUMN roles.code IS '角色编码';
+COMMENT ON COLUMN roles.name IS '角色名称';
+COMMENT ON COLUMN roles.descrition IS '角色简介';
+COMMENT ON COLUMN roles.created_time IS '创建时间';
+COMMENT ON COLUMN roles.updated_time IS '更新时间';
+COMMENT ON COLUMN roles.created_by IS '创建人';
+COMMENT ON COLUMN roles.updated_by IS '更新人';
+
+--用户和角色关系表
+DROP TABLE IF EXISTS users_roles_relation;
+CREATE TABLE users_roles_relation
+(
+  id           SERIAL PRIMARY KEY,
+  user_id      SERIAL       NOT NULL,
+  role_id      SERIAL       NOT NULL,
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('users_roles_relation_id_seq', 1000000);
+COMMENT ON TABLE users_roles_relation IS '用户和角色关系表';
+COMMENT ON COLUMN users_roles_relation.id IS '关系id';
+COMMENT ON COLUMN users_roles_relation.user_id IS '用户id';
+COMMENT ON COLUMN users_roles_relation.role_id IS '角色id';
+COMMENT ON COLUMN users_roles_relation.created_time IS '创建时间';
+COMMENT ON COLUMN users_roles_relation.updated_time IS '更新时间';
+COMMENT ON COLUMN users_roles_relation.created_by IS '创建人';
+COMMENT ON COLUMN users_roles_relation.updated_by IS '更新人';
+
+--菜单表
+DROP TABLE IF EXISTS menus;
+CREATE TABLE menus
+(
+  id           SERIAL PRIMARY KEY,
+  parent_id    SERIAL       NOT NULL,
+  type         VARCHAR(100),
+  href         VARCHAR(200),
+  icon         VARCHAR(200),
+  name         VARCHAR(200),
+  descrition   VARCHAR(500),
+  order_num    INTEGER,
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('menus_id_seq', 1000000);
+COMMENT ON TABLE menus IS '菜单表';
+COMMENT ON COLUMN menus.id IS '菜单id';
+COMMENT ON COLUMN menus.parent_id IS '父菜单id';
+COMMENT ON COLUMN menus.name IS '菜单名称';
+COMMENT ON COLUMN menus.type IS '菜单类型';
+COMMENT ON COLUMN menus.href IS '菜单路径';
+COMMENT ON COLUMN menus.icon IS '菜单图标';
+COMMENT ON COLUMN menus.descrition IS '菜单简介';
+COMMENT ON COLUMN menus.created_time IS '创建时间';
+COMMENT ON COLUMN menus.updated_time IS '更新时间';
+COMMENT ON COLUMN menus.created_by IS '创建人';
+COMMENT ON COLUMN menus.updated_by IS '更新人';
+
+--角色和菜单关系表
+DROP TABLE IF EXISTS roles_menus_relation;
+CREATE TABLE roles_menus_relation
+(
+  id           SERIAL PRIMARY KEY,
+  menu_id      SERIAL       NOT NULL,
+  role_id      SERIAL       NOT NULL,
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('roles_menus_relation_id_seq', 1000000);
+COMMENT ON TABLE roles_menus_relation IS '角色和菜单关系表';
+COMMENT ON COLUMN roles_menus_relation.id IS '关系id';
+COMMENT ON COLUMN roles_menus_relation.role_id IS '角色id';
+COMMENT ON COLUMN roles_menus_relation.menu_id IS '菜单id';
+COMMENT ON COLUMN roles_menus_relation.created_time IS '创建时间';
+COMMENT ON COLUMN roles_menus_relation.updated_time IS '更新时间';
+COMMENT ON COLUMN roles_menus_relation.created_by IS '创建人';
+COMMENT ON COLUMN roles_menus_relation.updated_by IS '更新人';
+
+--资源表
+DROP TABLE IF EXISTS resources;
+CREATE TABLE resources
+(
+  id           SERIAL PRIMARY KEY,
+  code         VARCHAR(100),
+  type         VARCHAR(100),
+  name         VARCHAR(200),
+  url          VARCHAR(200),
+  method       VARCHAR(20),
+  descrition   VARCHAR(500),
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('resources_id_seq', 1000000);
+COMMENT ON TABLE resources IS '资源表';
+COMMENT ON COLUMN resources.id IS '资源id';
+COMMENT ON COLUMN resources.name IS '资源名称';
+COMMENT ON COLUMN resources.descrition IS '资源简介';
+COMMENT ON COLUMN resources.created_time IS '创建时间';
+COMMENT ON COLUMN resources.updated_time IS '更新时间';
+COMMENT ON COLUMN resources.created_by IS '创建人';
+COMMENT ON COLUMN resources.updated_by IS '更新人';
+
+--角色和资源关系表
+DROP TABLE IF EXISTS roles_resources_relation;
+CREATE TABLE roles_resources_relation
+(
+  id           SERIAL PRIMARY KEY,
+  resource_id  SERIAL       NOT NULL,
+  role_id      SERIAL       NOT NULL,
+  created_time TIMESTAMP    NOT NULL DEFAULT now(),
+  updated_time TIMESTAMP    NOT NULL DEFAULT now(),
+  created_by   VARCHAR(100) NOT NULL,
+  updated_by   VARCHAR(100) NOT NULL
+);
+SELECT setval('roles_resources_relation_id_seq', 1000000);
+COMMENT ON TABLE roles_resources_relation IS '角色和资源关系表';
+COMMENT ON COLUMN roles_resources_relation.id IS '关系id';
+COMMENT ON COLUMN roles_resources_relation.role_id IS '角色id';
+COMMENT ON COLUMN roles_resources_relation.resource_id IS '资源id';
+COMMENT ON COLUMN roles_resources_relation.created_time IS '创建时间';
+COMMENT ON COLUMN roles_resources_relation.updated_time IS '更新时间';
+COMMENT ON COLUMN roles_resources_relation.created_by IS '创建人';
+COMMENT ON COLUMN roles_resources_relation.updated_by IS '更新人';
---
+++ auth/auth-oauth2/src/main/db/dml/users_dml.sql
@@ -0,0 +1,69 @@
+--用户
+INSERT INTO users (id, username, password, enabled, account_non_expired, credentials_non_expired, account_non_locked, name, mobile, created_time, updated_time, created_by, updated_by)
+VALUES
+  (101, 'admin', '$2a$10$vYA9wKn/hVGOtwQw2eHiceeIGNBdfLYpDmbzHgBSVmOfHXPH4iYdS', 'true', 'true', 'true', 'true',
+        '超级管理员', '', now(), now(), 'system', 'system'),
+  (102, 'zhoutaoo', '$2a$10$vYA9wKn/hVGOtwQw2eHiceeIGNBdfLYpDmbzHgBSVmOfHXPH4iYdS', 'true', 'true', 'true', 'true',
+        '周涛', 15619841000, now(), now(), 'system', 'system');
+--岗位
+INSERT INTO positions (id, name, descrition, created_time, updated_time, created_by, updated_by)
+VALUES (101, '首席执行官', '公司CEO，负责公司整体运转', now(), now(), 'system', 'system'),
+  (102, '首席运营官', '公司COO，负责公司整体运营', now(), now(), 'system', 'system'),
+  (103, '首席技术官', '公司CTO，负责公司整体运营', now(), now(), 'system', 'system');
+--角色
+INSERT INTO roles (id, code, name, descrition, created_time, updated_time, created_by, updated_by)
+VALUES (101, 'ADMIN', '超级管理员', '公司IT总负责人', now(), now(), 'system', 'system'),
+  (102, 'FIN', '财务', '财务', now(), now(), 'system', 'system'),
+  (103, 'IT', 'IT', 'IT角色', now(), now(), 'system', 'system');
+--用户组
+INSERT INTO groups (id, parent_id, name, descrition, created_time, updated_time, created_by, updated_by)
+VALUES (101, -1, '总公司', '总公司', now(), now(), 'system', 'system'),
+  (102, 101, '上海分公司', '上海分公司', now(), now(), 'system', 'system'),
+  (103, 102, '研发部门', '负责产品研发', now(), now(), 'system', 'system'),
+  (104, 102, '产品部门', '负责产品设计', now(), now(), 'system', 'system'),
+  (105, 102, '运营部门', '负责公司产品运营', now(), now(), 'system', 'system'),
+  (106, 102, '销售部门', '负责公司产品销售', now(), now(), 'system', 'system'),
+  (107, 101, '北京分公司', '北京分公司', now(), now(), 'system', 'system');
+--菜单
+INSERT INTO menus (id, parent_id, type, href, icon, name, descrition, order_num, created_time, updated_time, created_by, updated_by)
+VALUES (101, -1, 'MENU', '/admin', 'setting', '系统管理', '系统设置管理', 0, now(), now(), 'system', 'system'),
+  (102, 101, 'MENU', '/admin/users', 'fa-user', '用户管理', '用户新增，修改，查看，删除', 10, now(), now(), 'system', 'system'),
+  (103, 101, 'MENU', '/admin/menus', 'category', '菜单管理', '菜单新增，修改，删除', 20, now(), now(), 'system', 'system');
+--资源
+INSERT INTO resources (id, name, code, type, url, method, descrition, created_time, updated_time, created_by, updated_by)
+VALUES (101, '新增', 'user_manager:btn_add', 'button', '/users', 'POST', '新增用户功能', now(), now(), 'system', 'system'),
+  (102, '编辑', 'user_manager:btn_edit', 'button', '/users', 'PUT', '编辑用户功能', now(), now(), 'system', 'system'),
+  (103, '删除', 'user_manager:btn_del', 'button', '/users', 'DELETE', '删除用户功能', now(), now(), 'system', 'system'),
+  (104, '查看', 'user_manager:view', 'url', '/users', 'GET', '查询用户功能', now(), now(), 'system', 'system');
+
+--用户关系授权
+INSERT INTO users_roles_relation (id, user_id, role_id, created_time, updated_time, created_by, updated_by)
+VALUES (101, 101, 101, now(), now(), 'system', 'system'),
+  (102, 102, 101, now(), now(), 'system', 'system'),
+  (103, 102, 103, now(), now(), 'system', 'system');
+INSERT INTO users_groups_relation (id, user_id, group_id, created_time, updated_time, created_by, updated_by)
+VALUES (101, 101, 101, now(), now(), 'system', 'system'),
+  (102, 102, 101, now(), now(), 'system', 'system');
+INSERT INTO users_positions_relation (id, user_id, position_id, created_time, updated_time, created_by, updated_by)
+VALUES (101, 101, 103, now(), now(), 'system', 'system'),
+  (102, 102, 103, now(), now(), 'system', 'system');
+--角色关系表
+INSERT INTO roles_menus_relation (id, role_id, menu_id, created_time, updated_time, created_by, updated_by)
+VALUES (101, 101, 101, now(), now(), 'system', 'system'),
+  (102, 101, 102, now(), now(), 'system', 'system'),
+  (103, 101, 103, now(), now(), 'system', 'system'),
+  (104, 102, 101, now(), now(), 'system', 'system'),
+  (105, 102, 102, now(), now(), 'system', 'system'),
+  (106, 103, 101, now(), now(), 'system', 'system'),
+  (107, 103, 102, now(), now(), 'system', 'system'),
+  (108, 103, 103, now(), now(), 'system', 'system');
+INSERT INTO roles_resources_relation (id, role_id, resource_id, created_time, updated_time, created_by, updated_by)
+VALUES (101, 101, 101, now(), now(), 'system', 'system'),
+  (102, 101, 102, now(), now(), 'system', 'system'),
+  (103, 101, 103, now(), now(), 'system', 'system'),
+  (104, 101, 104, now(), now(), 'system', 'system'),
+  (105, 102, 104, now(), now(), 'system', 'system'),
+  (106, 103, 101, now(), now(), 'system', 'system'),
+  (107, 103, 102, now(), now(), 'system', 'system'),
+  (108, 103, 103, now(), now(), 'system', 'system'),
+  (109, 103, 104, now(), now(), 'system', 'system');
