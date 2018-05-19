12f7d0e64cc6e33df25719aee0afd942c1a7fd87
周涛 <zhoutaoo@foxmail.com>
2018-05-19 16:29:06 +0800

    调整sql语句格式

--- auth/authorization-server/src/main/java/com/springboot/auth/authorization/dao/UserMapper.java
+++ auth/authorization-server/src/main/java/com/springboot/auth/authorization/dao/UserMapper.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserMapper {
 
-    @Select("SELECT id,username,password,enabled,account_non_expired,credentials_non_expired,account_non_locked,name,mobile,created_time,updated_time,created_by,updated_by " +
-            "FROM users WHERE username = #{username}")
+    @Select("SELECT id,username,password,enabled,account_non_expired,credentials_non_expired,account_non_locked,name,mobile,created_time,updated_time,created_by,updated_by" +
+            " FROM users WHERE username = #{username}")
     User getByUsername(String username);
 }
