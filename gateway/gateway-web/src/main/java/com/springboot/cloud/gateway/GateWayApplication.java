2607cd462ffac49ad4e084afbd771474d730b7b5
周涛 <zhoutaoo@foxmail.com>
2018-05-27 18:56:44 +0800

    hytrix调整超时时间为3

--- gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/GateWayApplication.java
+++ gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/GateWayApplication.java
@@ -2,12 +2,14 @@ package com.springboot.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class GateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
