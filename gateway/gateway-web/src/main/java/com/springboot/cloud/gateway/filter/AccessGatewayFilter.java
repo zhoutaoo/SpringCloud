0e2554fe8d0199fff8cf501e88e5fd73700d76b8
周涛 <zhoutaoo@foxmail.com>
2018-05-27 15:39:04 +0800

    在调用签权服务前，网关先校验token是否有效，有效则不调用签权

--- gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/filter/AccessGatewayFilter.java
+++ gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/filter/AccessGatewayFilter.java
@@ -2,8 +2,11 @@ package com.springboot.cloud.gateway.filter;
 
 import com.springboot.cloud.core.entity.Result;
 import com.springboot.cloud.gateway.provider.AuthProvider;
+import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,7 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 
 /**
  * 主要负责jwt token的初步校验
@@ -26,6 +30,15 @@ public class AccessGatewayFilter implements GlobalFilter {
     AuthProvider authProvider;
 
     /**
+     * jwt token 密钥，主要用于token解析，签名验证
+     */
+    @Value("${spring.security.oauth2.jwt.signingKey}")
+    private String signingKey;
+
+    /**
+     * 1.首先网关检查token是否有效，无效直接返回401，不调用签权服务
+     * 2.调用签权服务器看是否对该请求有权限，有权限进入下一个filter，没有权限返回401
+     *
      * @param exchange
      * @param chain
      * @return
@@ -34,26 +47,53 @@ public class AccessGatewayFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
         String token = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        checkJwtAccessToken(token);
         String method = request.getMethodValue();
         String url = request.getPath().value();
         log.debug("url:{},method:{},headers:{}", url, method, request.getHeaders());
+        //无效token直接返回401
+        if (isInvalidJwtAccessToken(token)) {
+            return unauthorized(exchange);
+        }
         if (hasPermission(authProvider.auth(token, url, method))) {
             return chain.filter(exchange);
         }
         return unauthorized(exchange);
     }
 
+    /**
+     * 查看签权服务器返回结果，有权限返回true
+     *
+     * @param authResult
+     * @return
+     */
     private boolean hasPermission(Result authResult) {
         return authResult.isSuccess() && (boolean) authResult.getData();
     }
 
-    private void checkJwtAccessToken(String token) {
-        log.debug("check jwt token:{}", token);
+    /**
+     * 是否无效token
+     *
+     * @param token
+     * @return
+     */
+    private boolean isInvalidJwtAccessToken(String token) {
+        //是否无效true表示无效
+        boolean isInvalid = true;
+        if (StringUtils.isBlank(token))
+            return isInvalid;
+
+        try {
+            Jwt jwt = Jwts.parser().setSigningKey(signingKey).parse(token);
+            if (null != jwt)
+                isInvalid = false;
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException ex) {
+            log.warn("user token has expired or signature error ");
+        }
+        return isInvalid;
     }
 
     /**
-     * 网关
+     * 网关拒绝，返回401
      *
      * @param
      */
