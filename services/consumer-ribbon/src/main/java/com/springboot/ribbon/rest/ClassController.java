da8dff899cdec4c9b3ccfffb36bc59ef9a146c50
周涛 <zhoutaoo@foxmail.com>
2018-06-02 19:48:12 +0800

    调整ribbon，访问products服务

--- services/consumer-ribbon/src/main/java/com/springboot/ribbon/rest/ClassController.java
+++ services/consumer-ribbon/src/main/java/com/springboot/ribbon/rest/ClassController.java
@@ -1,23 +1,33 @@
 package com.springboot.ribbon.rest;
 
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import com.springboot.cloud.common.core.entity.vo.Result;
 import com.springboot.ribbon.service.ClassService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * Created by zhoutaoo on 21/07/2017.
- */
+import java.util.Map;
+
 @RestController
 public class ClassController {
 
     @Autowired
     ClassService classService;
 
-    @RequestMapping("/classes")
-    public Result hello() {
-        return classService.users();
+    @GetMapping("/classes")
+    public Result hello(@RequestParam String name) {
+        HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        Result users = classService.users(name);
+        context.close();
+        return users;
+    }
+
+    @PostMapping("/classes")
+    public Result hello(@RequestBody Map<String, String> params) {
+        HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        Result users = classService.users(params);
+        context.close();
+        return users;
     }
 
 }
--- services/consumer-ribbon/src/main/java/com/springboot/ribbon/service/ClassService.java
+++ services/consumer-ribbon/src/main/java/com/springboot/ribbon/service/ClassService.java
@@ -3,25 +3,36 @@ package com.springboot.ribbon.service;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.cache.annotation.CacheResult;
 import com.springboot.cloud.common.core.entity.vo.Result;
+import com.springboot.cloud.common.core.exception.ErrorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * Created by zhoutaoo on 23/07/2017.
- */
+import java.util.Map;
+
 @Service
 public class ClassService {
+
     @Autowired
     RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "usersFallback")
     @CacheResult
-    public Result users() {
-        return restTemplate.getForEntity("http://EUREKA-PRODUCER/users/?name=张三", Result.class).getBody();
+    public Result users(String name) {
+        return restTemplate.getForEntity("http://PRODUCER/products/?name={1}", Result.class, name).getBody();
     }
 
-    public Result usersFallback() {
-        return Result.fail();
+    @HystrixCommand(fallbackMethod = "usersFallback")
+    @CacheResult
+    public Result users(Map map) {
+        return restTemplate.postForEntity("http://PRODUCER/products/", map, Result.class).getBody();
+    }
+
+    public Result usersFallback(String name) {
+        return Result.fail(ErrorType.SYSTEM_BUSY);
+    }
+
+    public Result usersFallback(Map map) {
+        return Result.fail(ErrorType.SYSTEM_BUSY);
     }
 }
