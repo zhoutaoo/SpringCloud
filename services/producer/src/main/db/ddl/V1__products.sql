0a12ea5e8f03c6e463c2b67cb705c428001d0ea3
周涛 <zhoutaoo@foxmail.com>
2018-08-12 22:32:00 +0800

    产品demo独立数据库并修改表为product单数

--- services/producer/src/main/db/ddl/V1__products.sql
+++ services/producer/src/main/db/ddl/V1__products.sql
@@ -1,5 +1,10 @@
-DROP TABLE IF EXISTS products;
-CREATE TABLE products
+DROP DATABASE IF EXISTS sc_product;
+CREATE DATABASE sc_product;
+
+\c sc_product;
+
+DROP TABLE IF EXISTS product;
+CREATE TABLE product
 (
   id           SERIAL PRIMARY KEY,
   name         VARCHAR(200)            NOT NULL,
@@ -10,11 +15,11 @@ CREATE TABLE products
   updated_by   VARCHAR(100)            NOT NULL
 );
 
-COMMENT ON COLUMN products.id IS '编号';
-COMMENT ON COLUMN products.name IS '产品名称';
-COMMENT ON COLUMN products.description IS '产品描述';
-COMMENT ON COLUMN products.created_time IS '创建时间';
-COMMENT ON COLUMN products.updated_time IS '更新时间';
-COMMENT ON COLUMN products.created_by IS '创建人';
-COMMENT ON COLUMN products.updated_by IS '更新人';
+COMMENT ON COLUMN product.id IS '编号';
+COMMENT ON COLUMN product.name IS '产品名称';
+COMMENT ON COLUMN product.description IS '产品描述';
+COMMENT ON COLUMN product.created_time IS '创建时间';
+COMMENT ON COLUMN product.updated_time IS '更新时间';
+COMMENT ON COLUMN product.created_by IS '创建人';
+COMMENT ON COLUMN product.updated_by IS '更新人';
 
--- services/producer/src/main/java/com/springboot/services/producer/dao/ProductMapper.java
+++ services/producer/src/main/java/com/springboot/services/producer/dao/ProductMapper.java
@@ -12,24 +12,24 @@ import java.util.List;
 public interface ProductMapper {
 
     @Options(useGeneratedKeys = true)
-    @Insert("insert into products(name,description,updated_time,created_time,updated_by,created_by)" +
+    @Insert("insert into product(name,description,updated_time,created_time,updated_by,created_by)" +
             " values(#{name},#{description},now(),now(),#{updatedBy},#{createdBy})")
     long insert(Product product);
 
-    @Delete("delete from products where id=#{id}")
+    @Delete("delete from product where id=#{id}")
     void delete(long id);
 
-    @Update("update products set name=#{name},description=#{description},updated_by=#{updatedBy},updated_time=now()" +
+    @Update("update product set name=#{name},description=#{description},updated_by=#{updatedBy},updated_time=now()" +
             " where id=#{id}")
     void update(Product product);
 
     @Select("select id,name,description,updated_time,created_time,updated_by,created_by" +
-            " from products where id=#{id}")
+            " from product where id=#{id}")
     Product select(long id);
 
     @Select("<script>" +
             "select id,name,description,updated_time,created_time,updated_by,created_by" +
-            " from products" +
+            " from product" +
             " where name=#{name}" +
             "<if test='createdTimeStart!=null'>" +
             " and <![CDATA[created_time>#{createdTimeStart}]]>" +
