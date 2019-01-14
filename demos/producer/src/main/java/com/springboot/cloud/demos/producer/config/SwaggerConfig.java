e4438f66d532da78253a920f23853b3aac8049fc
周涛 <zhoutaoo@foxmail.com>
2019-01-15 00:01:07 +0800

    修改包名

--- demos/producer/src/main/java/com/springboot/cloud/demos/producer/config/SwaggerConfig.java
+++ demos/producer/src/main/java/com/springboot/cloud/demos/producer/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springboot.services.producer"))
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.cloud.demos.producer"))
                 .paths(PathSelectors.any())
                 .build();
     }
