d82c81a2d57bf491995355201726300d8af18306
周涛 <zhoutaoo@foxmail.com>
2019-01-26 11:48:49 +0800

    fix test擦色

--- gateway/gateway-web/src/test/java/com/springboot/cloud/gateway/GatewayApplicationTests.java
+++ gateway/gateway-web/src/test/java/com/springboot/cloud/gateway/GatewayApplicationTests.java
@@ -2,11 +2,13 @@ package com.springboot.cloud.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class GatewayApplicationTests {
 
     @Test
