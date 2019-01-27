153f070328dc36801c8f0a6d3a066bf228b1303f
周涛 <zhoutaoo@foxmail.com>
2019-01-27 22:15:41 +0800

    授权服务器异常接口统一响应报文

--- auth/authorization-server/src/test/java/com/springboot/auth/authorization/ApplicationTests.java
+++ auth/authorization-server/src/test/java/com/springboot/auth/authorization/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.springboot.auth.authorization;
 
+import com.springboot.auth.authorization.exception.AuthErrorType;
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,5 +12,10 @@ public class ApplicationTests {
     public void contextLoads() {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         System.out.println(passwordEncoder.encode("test_secret"));
+    }
+
+    @Test
+    public void testMethod() {
+        System.out.println(AuthErrorType.valueOf("SYSTEM_NO_PERMISSION").getCode());
     }
 }
