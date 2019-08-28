c4cab6d764265b2d33957a5e50be06058757dbc7
周涛 <zhoutaoo@foxmail.com>
2019-08-28 23:34:01 +0800

    默认数据库切换为mysql

--- auth/authentication-server/src/main/java/com/springboot/auth/authentication/config/WebServerSecurityConfig.java
+++ auth/authentication-server/src/main/java/com/springboot/auth/authentication/config/WebServerSecurityConfig.java
@@ -1,17 +1,14 @@
 package com.springboot.auth.authentication.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @Configuration
 @EnableWebSecurity
-@Order(1)
 public class WebServerSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
