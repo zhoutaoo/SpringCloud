223ec5e81f424d45b6fe7ddca76122063b4853ac
周涛 <zhoutaoo@foxmail.com>
2019-08-31 21:00:45 +0800

    优化auth代码

--- auth/authentication-server/src/main/java/com/springboot/cloud/auth/authentication/config/ResourceServerConfig.java
+++ auth/authentication-server/src/main/java/com/springboot/cloud/auth/authentication/config/ResourceServerConfig.java
@@ -3,7 +3,6 @@ package com.springboot.cloud.auth.authentication.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -23,11 +22,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         resourceServerSecurityConfigurer
                 .tokenStore(tokenStore())
                 .resourceId("WEBS");
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
     }
 
     @Bean
