f32ee3173bb3fb41c3f436b6689eebd3195c3eef
周涛 <zhoutaoo@foxmail.com>
2019-09-07 22:37:20 +0800

    修改各应用的注册中心配置

--- center/moss/src/main/java/com/springboot/cloud/MossApplication.java
+++ center/moss/src/main/java/com/springboot/cloud/MossApplication.java
@@ -1,15 +1,13 @@
-package com.springboot.turbine;
+package com.springboot.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableTurbineStream
-public class TurbineApplication {
+public class MossApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TurbineApplication.class, args);
+        SpringApplication.run(MossApplication.class, args);
     }
 }
