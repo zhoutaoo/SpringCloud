97c6cd24817683ea354531b28db5378118c2bae7
周涛 <zhoutaoo@foxmail.com>
2019-10-26 21:32:14 +0800

    使用mq实现网关路由修改时，实时同步到多个gateway-web节点，实时生效

---
+++ gateway/gateway-admin/src/main/java/com/springboot/cloud/gateway/admin/events/EventSender.java
@@ -0,0 +1,31 @@
+package com.springboot.cloud.gateway.admin.events;
+
+import com.springboot.cloud.gateway.admin.config.BusConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Slf4j
+public class EventSender {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private MessageConverter messageConverter;
+
+    @PostConstruct
+    public void init() {
+        rabbitTemplate.setMessageConverter(messageConverter);
+    }
+
+    public void send(String routingKey, Object object) {
+        log.info("routingKey:{}=>message:{}", routingKey, object);
+        rabbitTemplate.convertAndSend(BusConfig.EXCHANGE_NAME, routingKey, object);
+    }
+}
