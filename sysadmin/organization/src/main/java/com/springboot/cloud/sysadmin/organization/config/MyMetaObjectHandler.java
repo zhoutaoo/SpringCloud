6cbae728f7bc377dd4d7c36383e21733d9e33430
周涛 <zhoutaoo@foxmail.com>
2025-01-01 14:35:31 +0800

    delete project

--- sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/config/MyMetaObjectHandler.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.cloud.sysadmin.organization.config;
-
-import com.springboot.cloud.common.web.handler.PoMetaObjectHandler;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MyMetaObjectHandler extends PoMetaObjectHandler {
-
-}
